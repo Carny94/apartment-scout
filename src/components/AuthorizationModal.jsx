a54748ea5da1b79d68ed30cea82a70591e11342b
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2024-03-04 02:49:34 -0800

    connecting signup form from client side to server side in the utilities

--- src/components/AuthorizationModal.jsx
+++ src/components/AuthorizationModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import  { signUp } from '../../utilities/users-service'
 
 export default class AuthorizationModal extends React.Component {
     state = {
@@ -14,30 +15,63 @@ export default class AuthorizationModal extends React.Component {
           [e.target.name]: e.target.value,
           error: ''
         });
-    // handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     alert(JSON.stringify(this.state));
-    // }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        try {
+            const formData = {
+            name,
+            email,
+            password
+         }
+            const user = await signUp(formData);
+        }catch {
+            this.setState({ error: " sign up failed - try again "})
+        }
+    }
       };
 
   render() {
     const disable = this.state.password !== this.state.confirm;
     return (
-      <div>
+    <div>
      <div className="form-container">
         <form autoComplete="off" onSubmit={this.handleSubmit}>
           <label>name</label>
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+          <input 
+          type="text" 
+          name="name" 
+          value={this.state.name} 
+          onChange={this.handleChange} 
+          required />
+          
           <label>email</label>
-          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+          <input 
+          type="email" 
+          name="email" 
+          value={this.state.email} 
+          onChange={this.handleChange} 
+          required />
+          
           <label>password</label>
-          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+          <input 
+          type="password" 
+          name="password" 
+          value={this.state.password} 
+          onChange={this.handleChange} 
+          required />
+
           <label>confirm</label>
-          <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+          <input 
+          type="password" 
+          name="confirm" 
+          value={this.state.confirm} 
+          onChange={this.handleChange} 
+          required />
+          
           <button type="submit" disabled={disable}>SIGN UP</button>
         </form>
-      </div>
-      <p className="error-message">&nbsp;{this.state.error}</p>
+     </div>
+        <p className="error-message">&nbsp;{this.state.error}</p>
     </div>
     );
   }
---
+++ src/utilities/users-service.js
@@ -0,0 +1,7 @@
+import * as usersAPI from './users-api';
+
+
+export default function signUp(userData) {
+    const token = await usersAPI.signUp(userData)
+    return token;
+}
